6fbf9930a21118f77d84a9905d94559ee4dc1500
nico <nicolasmorales9413@gmail.com>
2024-05-06 23:36:28 -0300

    add NewTodoItem and ListTodoItem methods

--- ConsoleTodoList/Program.cs
+++ ConsoleTodoList/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleTodoList;
+var todoManager = new TodoManager();
 
-Console.WriteLine("Hello, World!");
+if (args.Length == 0)
+{
+    ShowHelp();
+    return;
+}
+
+string command = args[0];
+
+switch (command)
+{
+   case "--new":
+       NewTodoItem(todoManager, args);
+       break;
+   case "--list":
+       ListTodoItem(todoManager, args.Length > 1 ? args[1] : null);
+       break;
+}
+
+static void ShowHelp()
+{
+    Console.WriteLine("HERE GOES THE HELP");
+}
+
+static void NewTodoItem(TodoManager todoManager, string[] args)
+{
+    if (args.Length > 1)
+        todoManager.AddTodoItem(string.Join(" ", args.Skip(1)));
+    else
+        Console.WriteLine("Please provide a task.");
+}
+
+static void ListTodoItem(TodoManager todoManager, string filter)
+{
+    var todoItems = todoManager.GetTodoItems(filter);
+    foreach (var item in todoItems)
+    {
+        Console.WriteLine($"{item.Id}\t{item.Task}\t{(item.IsDone ? "Done" : "Pending")}");
+    }
+}
